66dce4b5e897e795ef23a11d07e6f32a4694f1e9
Areeviik <arevik.xachaturyan@gmail.com>
2022-07-10 20:06:20 +0400

    selection

--- SelectionSort.c
+++ SelectionSort.c
@@ -2,5 +2,27 @@
 
 void SelectionSort(int *arr)
 {
-    
+	size_t length;
+	int i;
+	int j;
+	int min;
+
+	length = sizeof(arr);
+
+	i = 0;
+	while (i < length)
+	{
+		min = i;
+		j = i + 1; 
+		while (j < length)
+		{
+			if (arr[j] < arr[min])
+				min = j;
+			++j;
+			if (min != i)
+				swap(&arr[i], &arr[min]);
+		}
+		++i;
+	}
+
 }
